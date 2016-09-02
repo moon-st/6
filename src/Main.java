5752e59900909a9097bf703f25987487c79c4139
moonst <bgyt@ya.ru>
2016-09-02 15:29:50 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,37 @@ public class Main {
             phCollection.addToCollection();
             }
         });
-//        menu.addEntry(new MenuEntry("2 - Обновить эл-т по индексу") {
+//        menu.addEntry(new MenuEntry("Обновить эл-т по индексу") {
 //            @Override
 //            public void run() {
 //
 //            }
 //        });
-//        menu.addEntry(new MenuEntry("3 - Удалить эл-т по индексу") {
+//        menu.addEntry(new MenuEntry("Удалить эл-т по индексу") {
+//            @Override
+//            public void run() {
+//
+//            }
+//        });
+//            menu.addEntry(new MenuEntry("Отсортировать по цене") {
+//            @Override
+//            public void run() {
+//
+//            }
+//        });
+//            menu.addEntry(new MenuEntry("Вычислить среднюю стоимость") {
+//            @Override
+//            public void run() {
+//
+//            }
+//        });
+//            menu.addEntry(new MenuEntry("Загрузить из файла") {
+//            @Override
+//            public void run() {
+//
+//            }
+//        });
+//            menu.addEntry(new MenuEntry("Выгрузить в файл") {
 //            @Override
 //            public void run() {
 //
