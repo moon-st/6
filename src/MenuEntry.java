8fde6386f5c6da59efd194c5e361fbaedc0d01a9
moonst <bgyt@ya.ru>
2016-09-02 15:22:55 +0300

    2

--- src/MenuEntry.java
+++ src/MenuEntry.java
@@ -1,6 +1,4 @@
-/**
- * Created by Admin on 02.09.2016.
- */
+
 public abstract class MenuEntry {
     private String title;
     public MenuEntry(String title){
--- src/PhCollection.java
+++ src/PhCollection.java
@@ -1,8 +1,4 @@
 import java.util.ArrayList;
-
-/**
- * Created by Admin on 02.09.2016.
- */
 public class PhCollection {
     ArrayList<Phototechnique> photoCollection = new ArrayList<>(100);
     public void addToCollection(){
