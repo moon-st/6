9f8add81f197a71b62ae294e48c70dc5dff0cd11
moon-st <bgyt@ya.ru>
2016-09-02 19:56:21 +0300

    Update PhCollection.java
    2

--- src/PhCollection.java
+++ src/PhCollection.java
@@ -1,14 +1,19 @@
 import java.util.ArrayList;
 public class PhCollection {
     ArrayList<Phototechnique> photoCollection = new ArrayList<>(100);
-    public void addToCollection(){
+    public void addCamToCollection(){
         Camera cam = new Camera();
         cam.SetPhotoObject();
         photoCollection.add(cam);
     }
+        public void addLensToCollection(){
+        Lens lens = new Lens();
+        lens.SetPhotoObject();
+        photoCollection.add(lens);
+    }
     public void PrintCollection(){
-        for (Phototechnique count : photoCollection){
-            count.PrintPhotoObject();
+        for (Phototechnique obj : photoCollection){
+            obj.PrintPhotoObject();
         }
     }
 }
